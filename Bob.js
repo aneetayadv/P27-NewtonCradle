0737beaf210fd6a7dfffa7ec6098c3967c53fe83
aneetayadv <anitayadav21@gmail.com>
2020-10-05 09:33:59 +0530

    Updating

--- Bob.js
+++ Bob.js
@@ -10,11 +10,11 @@ class Bob
 			}
 		this.x=x;
 		this.y=y;
-		this.r=r
-		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		this.r=r;
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options);
 		World.add(world, this.body);
 	}
-	
+
 	display()
 	{
 			
